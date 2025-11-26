c2ad803d8f652ad22de4756a9f58d8121ad5d354
Ethan Li <lietk12@gmail.com>
2025-11-26 15:30:02 +0100

    Try again to remove `forklift stage apply` in OS build since it fails

--- os-build/forklift/install.sh
+++ os-build/forklift/install.sh
@@ -68,22 +68,11 @@ forklift plt ls-img |
 
 # Prepare to apply the local pallet on the next boot
 # FIXME: containerd or runc always fails when we try to create containers (at least in a
-# systemd-nspawn container), for some reason, so `forklift stage apply` never actually completes
-# here. Nevertheless, we still need to run `forklift stage apply` here so that Docker will correctly
-# associate container image labels+tags to container images which are loaded just as a sha256 hash.
-
-# Make a temporary file which may be required by some Docker Compose apps in the pallet, just so
-# that those Compose apps can be successfully created (this is a rather dirty hack/workaround):
-echo "setup" | sudo tee /run/machine-name
+# systemd-nspawn container), so we can't run `forklift stage apply here`
 
 export FORKLIFT_STAGE_STORE=/var/lib/forklift/stages
-if ! sudo -E forklift stage apply; then
-  echo "Warning: the next staged pallet couldn't be successfully applied. We'll try again on the next boot, since the pallet might require some files which will only be created during the next boot."
-  # Reset the "apply-failed" status of the staged pallet to apply:
-  sudo -E forklift stage set-next --cache-img=false next
-  echo "Checking the plan for applying the staged pallet..."
-  sudo -E forklift stage plan
-fi
+sudo -E forklift stage plan
+sudo -E forklift stage set-next next
 
 # Use forklift on future boot sequences
 sudo systemctl preset forklift-apply.service
