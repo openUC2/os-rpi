524171f9652511f2aeeaf68af209860f7608ed80
Ethan Li <lietk12@gmail.com>
2025-12-09 13:16:13 +0100

    Prepopulate boot-init-root's directory structure for convenient overrides

--- os-build/init-root/prepare.sh
+++ os-build/init-root/prepare.sh
@@ -1,3 +1,19 @@
 #!/bin/bash -eux
 
 sudo mkdir -p /boot/init-root
+
+# Also set up some directory structure for easier usage:
+sudo mkdir -p /boot/init-root/home/pi/ImSwitchConfig/config
+sudo mkdir -p /boot/init-root/home/pi/ImSwitchConfig/imcontrol_setups
+sudo mkdir -p /boot/init-root/var/lib
+sudo mkdir -p /boot/init-root/etc/NetworkManager/system-connections-templates.d
+sudo mkdir -p /boot/init-root/etc/NetworkManager/system-connections.d
+sudo mkdir -p /boot/init-root/etc/NetworkManager/system-connections
+sudo mkdir -p /boot/init-root/etc/NetworkManager/dnsmasq-shared-templates.d
+sudo mkdir -p /boot/init-root/etc/NetworkManager/dnsmasq-shared.d
+sudo mkdir -p /boot/init-root/etc/cockpit/origins-templates.d
+sudo mkdir -p /boot/init-root/etc/cockpit/origins.d
+sudo mkdir -p /boot/init-root/etc/cockpit/cockpit.conf.d
+sudo mkdir -p /boot/init-root/etc/firewalld/policies
+sudo mkdir -p /boot/init-root/etc/firewalld/services
+sudo mkdir -p /boot/init-root/etc/firewalld/zones.d
