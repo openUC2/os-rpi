1a12203aa153bad22f46fd5ce7dac3a7bf88a80f
Ethan Li <lietk12@gmail.com>
2026-01-30 13:14:35 +0100

    Continue troubleshooting...

--- os-build/networking/install.sh
+++ os-build/networking/install.sh
@@ -23,10 +23,10 @@ sudo -E apt-get install -y -o Dpkg::Progress-Fancy=0 \
   rpi-usb-gadget
 mkdir -p /tmp/proc/device-tree
 echo "Raspberry Pi 5 Model B Rev 1.1" >/tmp/proc/device-tree/model
-sudo mount -o ro,bind /tmp/proc/device-tree /proc/device-tree
+sudo mount -o ro,bind /tmp/proc /proc
 sudo cat /proc/device-tree/model
 sudo rpi-usb-gadget on
-sudo umount /proc/device-tree/model
+sudo umount /proc
 
 # Install tailscale
 sudo -E apt-get install -y -o Dpkg::Progress-Fancy=0 \
