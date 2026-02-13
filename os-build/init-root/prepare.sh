281a635ed6fb5223c64a24f7c8f2ce8f5495548e
Ethan Li <lietk12@gmail.com>
2026-02-13 17:35:40 +0100

    Support pi file ownership in boot-init-root from directories

--- os-build/init-root/prepare.sh
+++ os-build/init-root/prepare.sh
@@ -1,21 +1,22 @@
 #!/bin/bash -eux
 
-sudo mkdir -p /boot/firmware/init-root
+sudo mkdir -p /boot/firmware/init-root/as-root
+sudo mkdir -p /boot/firmware/init-root/as-pi
 
 # Also set up some directory structure for easier usage:
-sudo mkdir -p /boot/firmware/init-root/boot/firmware
-sudo mkdir -p /boot/firmware/init-root/usr/
-sudo mkdir -p /boot/firmware/init-root/etc/NetworkManager/system-connections-templates.d
-sudo mkdir -p /boot/firmware/init-root/etc/NetworkManager/system-connections.d
-sudo mkdir -p /boot/firmware/init-root/etc/NetworkManager/system-connections
-sudo mkdir -p /boot/firmware/init-root/etc/NetworkManager/dnsmasq-shared-templates.d
-sudo mkdir -p /boot/firmware/init-root/etc/NetworkManager/dnsmasq-shared.d
-sudo mkdir -p /boot/firmware/init-root/etc/cockpit/origins-templates.d
-sudo mkdir -p /boot/firmware/init-root/etc/cockpit/origins.d
-sudo mkdir -p /boot/firmware/init-root/etc/cockpit/cockpit.conf.d
-sudo mkdir -p /boot/firmware/init-root/etc/firewalld/policies
-sudo mkdir -p /boot/firmware/init-root/etc/firewalld/services
-sudo mkdir -p /boot/firmware/init-root/etc/firewalld/zones.d
-sudo mkdir -p /boot/firmware/init-root/var/lib
-sudo mkdir -p /boot/firmware/init-root/home/pi/ImSwitchConfig/config
-sudo mkdir -p /boot/firmware/init-root/home/pi/ImSwitchConfig/imcontrol_setups
+sudo mkdir -p /boot/firmware/init-root/as-root/boot/firmware
+sudo mkdir -p /boot/firmware/init-root/as-root/usr/
+sudo mkdir -p /boot/firmware/init-root/as-root/etc/NetworkManager/system-connections-templates.d
+sudo mkdir -p /boot/firmware/init-root/as-root/etc/NetworkManager/system-connections.d
+sudo mkdir -p /boot/firmware/init-root/as-root/etc/NetworkManager/system-connections
+sudo mkdir -p /boot/firmware/init-root/as-root/etc/NetworkManager/dnsmasq-shared-templates.d
+sudo mkdir -p /boot/firmware/init-root/as-root/etc/NetworkManager/dnsmasq-shared.d
+sudo mkdir -p /boot/firmware/init-root/as-root/etc/cockpit/origins-templates.d
+sudo mkdir -p /boot/firmware/init-root/as-root/etc/cockpit/origins.d
+sudo mkdir -p /boot/firmware/init-root/as-root/etc/cockpit/cockpit.conf.d
+sudo mkdir -p /boot/firmware/init-root/as-root/etc/firewalld/policies
+sudo mkdir -p /boot/firmware/init-root/as-root/etc/firewalld/services
+sudo mkdir -p /boot/firmware/init-root/as-root/etc/firewalld/zones.d
+sudo mkdir -p /boot/firmware/init-root/as-root/var/lib
+sudo mkdir -p /boot/firmware/init-root/as-pi/home/pi/ImSwitchConfig/config
+sudo mkdir -p /boot/firmware/init-root/as-pi/home/pi/ImSwitchConfig/imcontrol_setups
