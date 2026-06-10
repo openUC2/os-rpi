464b00574a38b5ad663d42309b3b701e150cb8c3
Ethan Li <lietk12@gmail.com>
2026-06-10 18:44:11 +0200

    Try adding udev rules for Daheng cameras and Hikrobot cameras in the pallet

--- os-build/imswitch-hardware/install.sh
+++ os-build/imswitch-hardware/install.sh
@@ -1,25 +1,3 @@
 #!/bin/bash -eu
 
 config_files_root=$(dirname "$(realpath "$BASH_SOURCE")")
-
-# Install HIK camera driver stuff
-# TODO: we probably only need the udev rules made by this, and maybe also some sysctl configs, and
-# maybe also a script (which we'd run as a systemd service), in which case we should deliver those
-# just via Forklift instead of baking them into the base OS image via a DEB package.
-
-ARCH=$(uname -m)
-if [ "$ARCH" = "aarch64" ]; then
-  HIK_ARCH="aarch64"
-  HIK_SAMPLE_PATH="aarch64"
-elif [ "$ARCH" = "x86_64" ]; then
-  HIK_ARCH="x86_64"
-  HIK_SAMPLE_PATH="64"
-else
-  echo "Unsupported architecture: $ARCH"
-  exit 1
-fi
-
-HIK_DEB_FILE="$(mktemp --suffix=".deb")"
-curl -L "https://github.com/openUC2/ImSwitchDockerInstall/releases/download/imswitch-master/MVS-3.0.1_${HIK_ARCH}_20241128.deb" \
-  >"$HIK_DEB_FILE"
-sudo dpkg -i "$HIK_DEB_FILE"
