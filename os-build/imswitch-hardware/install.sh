ae6c7cab956544c0d16071e3fd2cfde9091bc1c7
Ethan Li <lietk12@gmail.com>
2026-06-10 22:39:33 +0200

    Run ImSwitch without privileged access (#247)
    This PR integrates https://github.com/openUC2/ImSwitch/pull/291 .
    
    This PR also removes the `privileged: true` setting in the Docker
    Compose file for the pallet's ImSwitch deployment.
    
    This PR also removes installation of the Hikrobot camera driver in the
    OS build scripts, as only the udev rules and kernel parameters are
    needed (and those are now delivered through the pallet's ImSwitch
    deployment).
    
    This work is tracked on Notion at
    https://app.notion.com/p/When-ImSwitch-edits-an-imcontrol_setups-file-it-changes-file-ownership-to-root-3754e612c78a806582a3e6814e4a86d8?source=copy_link

--- os-build/imswitch-hardware/install.sh
+++ os-build/imswitch-hardware/install.sh
@@ -2,28 +2,6 @@
 
 config_files_root=$(dirname "$(realpath "$BASH_SOURCE")")
 
-# Install HIK camera driver stuff
-# TODO: we probably only need the udev rules made by this, and maybe also some sysctl configs, and
-# maybe also a script (which we'd run as a systemd service), in which case we should deliver those
-# just via Forklift instead of baking them into the base OS image via a DEB package.
-
-ARCH=$(uname -m)
-if [ "$ARCH" = "aarch64" ]; then
-  HIK_ARCH="aarch64"
-  HIK_SAMPLE_PATH="aarch64"
-elif [ "$ARCH" = "x86_64" ]; then
-  HIK_ARCH="x86_64"
-  HIK_SAMPLE_PATH="64"
-else
-  echo "Unsupported architecture: $ARCH"
-  exit 1
-fi
-
-HIK_DEB_FILE="$(mktemp --suffix=".deb")"
-curl -L "https://github.com/openUC2/ImSwitchDockerInstall/releases/download/imswitch-master/MVS-3.0.1_${HIK_ARCH}_20241128.deb" \
-  >"$HIK_DEB_FILE"
-sudo dpkg -i "$HIK_DEB_FILE"
-
 # Add config.txt configuration for the CAN transceiver on the openUC2 HAT+
 file="/boot/firmware/config.txt"
 sudo bash -c "cat \"$config_files_root$file.snippet\" >> \"$file\""
