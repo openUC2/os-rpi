openUC2/os-rpi

commit ae6c7cab956544c0d16071e3fd2cfde9091bc1c7

Run ImSwitch without privileged access (#247)
This PR integrates https://github.com/openUC2/ImSwitch/pull/291 .

This PR also removes the `privileged: true` setting in the Docker
Compose file for the pallet's ImSwitch deployment.

This PR also removes installation of the Hikrobot camera driver in the
OS build scripts, as only the udev rules and kernel parameters are
needed (and those are now delivered through the pallet's ImSwitch
deployment).

This work is tracked on Notion at
https://app.notion.com/p/When-ImSwitch-edits-an-imcontrol_setups-file-it-changes-file-ownership-to-root-3754e612c78a806582a3e6814e4a86d8?source=copy_link

os-build/imswitch-hardware/install.sh
@@ -2,28 +2,6 @@
 
 config_files_root=$(dirname "$(realpath "$BASH_SOURCE")")
 
-# Install HIK camera driver stuff
-# TODO: we probably only need the udev rules made by this, and maybe also some sysctl configs, and
-# maybe also a script (which we'd run as a systemd service), in which case we should deliver those
-# just via Forklift instead of baking them into the base OS image via a DEB package.
-
-ARCH=$(uname -m)
-if [ "$ARCH" = "aarch64" ]; then
-  HIK_ARCH="aarch64"
-  HIK_SAMPLE_PATH="aarch64"
-elif [ "$ARCH" = "x86_64" ]; then
-  HIK_ARCH="x86_64"
-  HIK_SAMPLE_PATH="64"
-else
-  echo "Unsupported architecture: $ARCH"
-  exit 1
-fi
-
-HIK_DEB_FILE="$(mktemp --suffix=".deb")"
-curl -L "https://github.com/openUC2/ImSwitchDockerInstall/releases/download/imswitch-master/MVS-3.0.1_${HIK_ARCH}_20241128.deb" \
-  >"$HIK_DEB_FILE"
-sudo dpkg -i "$HIK_DEB_FILE"
-
 # Add config.txt configuration for the CAN transceiver on the openUC2 HAT+
 file="/boot/firmware/config.txt"
 sudo bash -c "cat \"$config_files_root$file.snippet\" >> \"$file\""

deployments/imswitch.deploy.yml
@@ -4,5 +4,7 @@ features:
   - frontend-untrusted
   - firewall-allow-direct
   - firewall-allow-public
-  - can-device
+  - hikrobot-cameras
+  - daheng-cameras
+  - can-interface
 disabled: false

deployments/imswitch.pkg/can-interface.compose.yml
@@ -1,0 +1,6 @@
+services:
+  server:
+    device_cgroup_rules:
+      - 'a 153:* rwm' # SPI devices, e.g. /dev/spidev0.1
+    group_add:
+      - spi

deployments/imswitch.pkg/deployment.compose.yml
@@ -1,17 +1,33 @@
 services:
   server:
-    image: ghcr.io/openuc2/imswitch:sha-1af4c81@sha256:5551b925fd0a0e7171202718b2cdd0745ffd404eb75ac0d5e3bc50ee887c4be0
-    privileged: true
+    image: ghcr.io/openuc2/imswitch:sha-84f5353@sha256:3dbf37c91d49e6de528a474184ab91b3e7f833f2e18609c8383c1954c0a16590
     volumes:
       - /etc/machine-id:/etc/machine-id:ro
       - /home/pi/ImSwitchConfig:/home/pi/ImSwitchConfig
       - /home/pi/Datasets:/home/pi/Datasets
       - /media:/media
       - /run/udev:/run/udev:ro
-      # For scanning and accessing USB serial devices:
       - /dev:/dev
-      # For libcamera:
-      - /dev/dma_heap:/dev/dma_heap
+    device_cgroup_rules:
+      - 'a 188:* rwm' # USB serial devices (such as the openUC2 HAT+), e.g. /dev/ttyUSB0
+      # - 'a 204:* rwm' # Serial devices, e.g. /dev/ttyAMA0
+      - 'a 81:* rwm' # libcamera-based cameras, e.g. /dev/video0
+      - 'a 511:* rwm' # libcamera-based cameras, e.g. /dev/media0
+      # - 'a 89:* rwm' # I2C devices, e.g. /dev/i2c-1
+      # - 'a 254:* rwm' # GPIO devices, e.g. /dev/gpiochip0
+      # - 'a 235:* rwm' # GPIO devices, e.g. /dev/gpiomem0
+      # - 'a 239:* rwm' # GPIO devices, e.g. /dev/gpiomem1
+      # - 'a 238:* rwm' # GPIO devices, e.g. /dev/gpiomem2
+      # - 'a 237:* rwm' # GPIO devices, e.g. /dev/gpiomem3
+      # - 'a 236:* rwm' # GPIO devices, e.g. /dev/gpiomem4
+      # - 'a *:* rwm' # allow all devices; only use this for debugging! Don't use this in production, because it's a massive security backdoor!
+    group_add:
+      - tty
+      - dialout
+      - video
+      - plugdev
+      # - i2c
+      # - gpio
     environment:
       - HEADLESS=1
       - HTTP_PORT=8001

deployments/imswitch.pkg/forklift-package.yml
@@ -114,8 +114,24 @@ features:
           target: overlays/etc/firewalld/services/imswitch-dev.xml
         - description: Drop-in XML fragment for firewalld's imswitch-dev service
           target: overlays/etc/firewalld/zones.d/public/70-service-imswitch-dev.xml
-  can-device:
+  hikrobot-cameras:
+    description: Support for Hikrobot cameras
+    provides:
+      file-exports:
+        - description: Kernel parameters
+          target: overlays/etc/sysctl.d/80-hikrobot-cameras.conf
+        - description: udev rules
+          target: overlays/usr/lib/udev/rules.d/80-hikrobot-cameras.rules
+  daheng-cameras:
+    description: Support for Daheng cameras
+    provides:
+      file-exports:
+        - description: udev rules
+          target: overlays/usr/lib/udev/rules.d/80-daheng-cameras.rules
+  can-interface:
     description: Provides automatic enablement of the OS interface for an SPI CAN bus transceiver
+    compose-files:
+      - can-interface.compose.yml
     provides:
       file-exports:
         - description: systemd-networkd configuration for the can0 device

deployments/imswitch.pkg/overlays/etc/sysctl.d/80-hikrobot-cameras.conf
@@ -1,0 +1,6 @@
+# From MVS-3.0.1_aarch64_20241128.deb/data.tar.xz/opt/MVClient_Install_Temp/set_rp_filter.sh
+net.ipv4.conf.default.rp_filter = 0
+
+# From MVS-3.0.1_aarch64_20241128.deb/data.tar.xz/opt/MVClient_Install_Temp/set_socket_buffer_size.sh
+net.core.rmem_max = 10485760
+net.core.wmem_max = 10485760

deployments/imswitch.pkg/overlays/usr/lib/udev/rules.d/80-daheng-cameras.rules
@@ -1,0 +1,1 @@
+ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="2ba2", MODE:="0666", TAG+="uaccess", TAG+="udev-acl", GROUP="plugdev"

deployments/imswitch.pkg/overlays/usr/lib/udev/rules.d/80-hikrobot-cameras.rules
@@ -1,0 +1,2 @@
+ACTION=="add", SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTRS{idVendor}=="2bdf", MODE="0666", GROUP="plugdev"
+ACTION=="add", KERNEL=="ttyvirserial[0-9]*", MODE="0666", GROUP="plugdev"

commit 4c56bfed3d44a12bab08f0e21a4668b11cb3b6a3

Undo regression from #247 in hikrobot camera support (#260)
PR #247 stopped installing the Hikrobot camera driver (MVS-3.0.1) in the
host OS. This caused ImSwitch-in-Docker to stop being able to use
Hikrobot cameras in fresh OS installations, even though #247 added
host-OS deployment of the udev rules needed for the Hikrobot camera. It
seems that the Hikrobot camera driver is needed for installing various
library files into `/opt/MVS` (which is outside what Forklift manages),
and it may also be needed for installing an MVS camera server which
appears to be automatically started during boot.

os-build/imswitch-hardware/install.sh
@@ -2,6 +2,28 @@
 
 config_files_root=$(dirname "$(realpath "$BASH_SOURCE")")
 
+# Install HIK camera driver stuff
+# Note: this adds some library files into `/opt/MVS` which are needed for ImSwitch-in-Docker to
+# work, and it also installs a server launched during boot which may also be needed for
+# ImSwitch-to-Docker to work.
+
+ARCH=$(uname -m)
+if [ "$ARCH" = "aarch64" ]; then
+  HIK_ARCH="aarch64"
+  HIK_SAMPLE_PATH="aarch64"
+elif [ "$ARCH" = "x86_64" ]; then
+  HIK_ARCH="x86_64"
+  HIK_SAMPLE_PATH="64"
+else
+  echo "Unsupported architecture: $ARCH"
+  exit 1
+fi
+
+HIK_DEB_FILE="$(mktemp --suffix=".deb")"
+curl -L "https://github.com/openUC2/ImSwitchDockerInstall/releases/download/imswitch-master/MVS-3.0.1_${HIK_ARCH}_20241128.deb" \
+  >"$HIK_DEB_FILE"
+sudo dpkg -i "$HIK_DEB_FILE"
+
 # Add config.txt configuration for the CAN transceiver on the openUC2 HAT+
 file="/boot/firmware/config.txt"
 sudo bash -c "cat \"$config_files_root$file.snippet\" >> \"$file\""